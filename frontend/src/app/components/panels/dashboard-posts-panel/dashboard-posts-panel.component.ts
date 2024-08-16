c3498617fdb125a626037b1951fa70ecba4ae896
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-08-16 11:36:45 -0600

    Panel de Posts

--- frontend/src/app/components/panels/dashboard-posts-panel/dashboard-posts-panel.component.ts
+++ frontend/src/app/components/panels/dashboard-posts-panel/dashboard-posts-panel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardPostCardComponent } from "../../templates/dashboard-post-card/dashboard-post-card.component";
 
 @Component({
   selector: 'app-dashboard-posts-panel',
   standalone: true,
-  imports: [],
+  imports: [DashboardPostCardComponent],
   templateUrl: './dashboard-posts-panel.component.html',
   styleUrl: './dashboard-posts-panel.component.css'
 })
