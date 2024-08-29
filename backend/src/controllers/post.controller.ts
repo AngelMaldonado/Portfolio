6aa406b40b75800e95a9bc448146779ba60996d9
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-08-29 17:17:57 -0600

    Ruteando correctamente vista de post por id

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -12,10 +12,19 @@ class PostController {
     }
   }
 
+  async Get(req: Request, res: Response) {
+    try {
+      const post = await Post.findById(req.params.id)
+      res.status(200).send(post)
+    } catch (error: unknown) {
+      throw new Error(error as string)
+    }
+  }
+
   async All(req: Request, res: Response) {
     try {
-      const post = await Post.find()
-      res.status(200).send(post)
+      const posts = await Post.find()
+      res.status(200).send(posts)
     } catch (error: unknown) {
       throw new Error(error as string)
     }
--- frontend/src/app/screens/blog-post/blog-post.component.ts
+++ frontend/src/app/screens/blog-post/blog-post.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { BadgeComponent } from '../../components/atoms/badge/badge.component';
 import { StarsRatingComponent } from '../../components/atoms/stars-rating/stars-rating.component';
-import { RouterLink } from '@angular/router';
-import { Location } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DatePipe, Location } from '@angular/common';
+import Post from '../../models/Post';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-blog-post',
   standalone: true,
-  imports: [BadgeComponent, StarsRatingComponent, RouterLink],
+  imports: [BadgeComponent, StarsRatingComponent, RouterLink, DatePipe],
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.css'
 })
 export class BlogPostComponent {
-  constructor(private location: Location) { }
+  post: Post = new Post("", "", "", "", "")
+
+  constructor(private location: Location, private activatedRoute: ActivatedRoute, private postService: PostsService) {
+    postService.getPost(this.activatedRoute.snapshot.params['id'])
+      .subscribe(post => this.post = post)
+  }
 
   goBack() {
     this.location.back();
